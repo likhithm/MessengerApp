e9b88903466906d4c7e2eb918fec92345de7c1f7
Your Name <likhithgowda1996@gmail.com>
2019-04-18 14:39:08 -0700

    added new widget

--- lib/data_viz/data_v_screen.dart
+++ lib/data_viz/data_v_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:app_usage/app_usage.dart';
+import 'package:Connect/data_viz/widgets/app_usage.dart';
+
 
 
 class DataViz extends StatefulWidget {
@@ -15,6 +18,7 @@ class DataVizState extends State<DataViz> {
   @override
   void initState() {
     super.initState();
+    getUsageStats();
 
   }
 
@@ -30,7 +34,31 @@ class DataVizState extends State<DataViz> {
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color:Colors.lightBlue),
       ),
-      body: new Container()
+      body: AppUsageChart()
     );
+  }
+
+  void getUsageStats() async {
+    // Initialization
+    AppUsage appUsage = new AppUsage();
+    try {
+      // Define a time interval
+      DateTime endDate = new DateTime.now();
+      print(endDate);
+      DateTime startDate = DateTime(endDate.year, endDate.month, endDate.day, 0 , 0 , 0);
+      print(startDate);
+
+      // Fetch the usage stats
+      Map<String, double> usage = await appUsage.fetchUsage(startDate, endDate);
+
+      // (Optional) Remove entries for apps with 0 usage time
+      usage.removeWhere((key,val) => val == 0);
+
+
+      print(usage['com.messenger.app']);
+    }
+    on AppUsageException catch (exception) {
+      print(exception);
+    }
   }
 }
---
+++ lib/data_viz/widgets/app_usage.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+
+class AppUsageChart extends StatefulWidget {
+
+  @override
+  AppUsageChartState createState() => new AppUsageChartState();
+}
+
+class ClicksPerYear {
+  final String year;
+  final int clicks;
+  final charts.Color color;
+
+  ClicksPerYear(this.year, this.clicks, Color color)
+      : this.color = new charts.Color(
+      r: color.red, g: color.green, b: color.blue, a: color.alpha);
+}
+
+class AppUsageChartState extends State<AppUsageChart> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var data = [
+      new ClicksPerYear('January', 738, Colors.red),
+      new ClicksPerYear('Febrauary', 1443, Colors.yellow),
+      new ClicksPerYear('March', 234, Colors.green),
+      new ClicksPerYear('April', 123, Colors.blue),
+    ];
+
+    var series = [
+      new charts.Series(
+        domainFn: (ClicksPerYear clickData, _) => clickData.year,
+        measureFn: (ClicksPerYear clickData, _) => clickData.clicks,
+        colorFn: (ClicksPerYear clickData, _) => clickData.color,
+        id: 'Clicks',
+        data: data,
+      ),
+    ];
+
+    var chart = new charts.BarChart(
+      series,
+      animate: true,
+    );
+    var chartWidget = new Padding(
+      padding: new EdgeInsets.all(32.0),
+      child: new SizedBox(
+        height: 200.0,
+        child: chart,
+      ),
+    );
+
+    return new Scaffold(
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'App usage statistics',
+            ),
+           /* new Text(
+              '$_counter',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .display1,
+            ),*/
+            chartWidget,
+          ],
+        ),
+      ),
+     /* floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),*/
+    );
+  }
+
+}
